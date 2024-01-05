f867bd6550019f0b7084508090f217fd15bbbec2
jomsey <muwanguzijoseph75@gmail.com>
2024-01-05 13:18:57 +0300

    some changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,17 +5,15 @@ import Footer from '@/components/footer/footer'
 import Back2top from '@/components/back-to-top-button/b2top'
 import Contact from '@/components/contacts/contacts'
 import { usePathname } from 'next/navigation'
-import { Lato } from 'next/font/google'
 
 
-const lato = Lato({ subsets: ['latin'], weight: ["100", "700", "900"] })
 
 export default function RootLayout({ children }) {
   const path = usePathname()
 
   return (
     <html lang="en" className='scroll-smooth relative' id='top'>
-      <body className={`bg-white scroll-smooth ${lato.className}`}>
+      <body className={`bg-white scroll-smooth`}>
         {!path.toLocaleLowerCase().search('/admin') ?
           <>{children}</> :
           <>
